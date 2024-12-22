fe7bc6ee27cdf933cfcc50f647b1e72a7be259e7
Guru <guru85111@gmail.com>
2024-12-22 16:27:32 +0530

    `Remove server headers from Vite config`

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,11 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    headers: {
-      "Cross-Origin-Opener-Policy": "same-origin-allow-popups", // Ensures same-origin interaction
-      "Cross-Origin-Embedder-Policy": "require-corp", // Ensures content is safe for embedding
-    },
-    
-  },
 });
