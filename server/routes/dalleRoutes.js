af9336c5d558dfe69e32d66e6a358784bdc33e31
Guru <guru85111@gmail.com>
2024-12-23 23:14:26 +0530

    `Remove duplicate CORS configuration in server and dalleRoutes files`

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -8,12 +8,7 @@ app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 dotenv.config();
 
-app.use(
-  cors({
-    origin:
-      "https://dalle-2-0-frontend.vercel.app",
-  })
-);
+app.use(cors({origin:"https://dalle-2-0-frontend.vercel.app"}));
 
 const router = express.Router();
 
